5f343fc54bca6da9fe0cd53dc66d21d8fac8f910
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-29 11:36:40 +0100

    nullptr & enums

--- core-features/tests_enumerations.cpp
+++ core-features/tests_enumerations.cpp
@@ -5,7 +5,31 @@
 #include <string>
 #include <vector>
 
-enum class Coffee
+enum OldStyleDaysOfWeek : uint8_t
+{
+    Mon = 1,
+    Tue,
+    Wed,
+    Thd,
+    Fri,
+    Sat,
+    Sun
+};
+
+static_assert(sizeof(OldStyleDaysOfWeek) == 1);
+
+TEST_CASE("old style enums")
+{
+    OldStyleDaysOfWeek day_of_week = Mon;
+    day_of_week = static_cast<OldStyleDaysOfWeek>(5);
+    REQUIRE(day_of_week == Fri);
+
+    std::underlying_type_t<OldStyleDaysOfWeek> value = day_of_week; // C++11
+
+    auto integer_value = std::to_underlying(day_of_week); // C++23
+}
+
+enum class Coffee : uint8_t
 {
     espresso,
     cappuccino,
@@ -15,9 +39,90 @@ enum class Coffee
     chemex
 };
 
+namespace Cpp23
+{
+    template <typename TEnum>
+    constexpr auto to_underlying(TEnum e) noexcept
+    {
+        return static_cast<std::underlying_type_t<TEnum>>(e);
+    }
+}
+
 TEST_CASE("enum classes")
 {
     Coffee coffee = Coffee::v60;
 
-    REQUIRE(static_cast<int>(coffee) == 4);
+    coffee = static_cast<Coffee>(4);
+    REQUIRE(coffee == Coffee::v60);
+
+    auto value = static_cast<std::underlying_type_t<Coffee>>(coffee);
+    REQUIRE(static_cast<std::underlying_type_t<Coffee>>(coffee) == 4);
+    REQUIRE(Cpp23::to_underlying(coffee) == 4);
+}
+
+namespace [[deprecated]] Legacy
+{
+    namespace OldStyleDaysOfWeekAlt
+    {
+        enum type : uint32_t
+        {
+            Mon = 1,
+            Tue,
+            Wed,
+            Thd,
+            Fri,
+            Sat,
+            Sun
+        };
+    }
+}
+
+namespace SinceCpp11
+{
+    enum class OldStyleDaysOfWeek : uint8_t
+    {
+        Mon = 1,
+        Tue,
+        Wed,
+        Thd,
+        Fri,
+        Sat,
+        Sun
+    };
+}
+
+TEST_CASE("since C++17")
+{
+    Coffee coffee{4};
+    REQUIRE(coffee == Coffee::v60);
+}
+
+namespace StdExplain
+{
+    enum byte : uint8_t
+    {
+    };
+
+    byte operator&(byte lhs, byte rhs)
+    {
+        return static_cast<byte>(static_cast<uint8_t>(lhs) & static_cast<uint8_t>(rhs));
+    }
+}
+
+TEST_CASE("char as bad byte")
+{
+    char c1 = 65;
+    char c2 = 3;
+
+    std::cout << (c1 + c2) << "\n";
+    char result = ~(c1 & c2);
+
+    std::cout << "result: " << result << "\n";
+}
+
+TEST_CASE("std::byte")
+{
+    std::byte b1{0b0000'00100};
+    std::byte b2{42};
+    std::cout << "Result: " << std::to_integer<int>(~(b1 & b2)) << "\n";
 }
--- core-features/tests_nullptr.cpp
+++ core-features/tests_nullptr.cpp
@@ -7,7 +7,7 @@
 
 void foo(int* ptr)
 {
-    if (ptr)
+    if (ptr != nullptr)
     {
         std::cout << "foo(ptr is not null)\n";
     }
@@ -17,7 +17,60 @@ void foo(int* ptr)
     }
 }
 
+void foo(int value)
+{
+    std::cout << "foo(int)\n";
+}
+
+void foo(nullptr_t)
+{
+    std::cout << "foo(nullptr_t)\n";
+}
+
 TEST_CASE("nullptr")
 {
-    foo(NULL);
+    foo(nullptr);
+    
+    int* ptr = nullptr;
+    foo(ptr);
+
+    int* ptr_other{}; // ptr_other = nullptr
+    REQUIRE(ptr_other == nullptr);
+}
+
+void print(std::string_view text)
+{
+    std::cout << "Text: " << text << "\n";
+}
+
+TEST_CASE("string literals")
+{
+    using namespace std::literals;
+
+    const char* ctxt = R"(abcdef)"
+"ghik";
+    std::string str_txt = "abcdef";
+
+    print(ctxt);
+
+    auto atxt_1 = "abcd\0ef";   // const char*
+    auto atxt_2 = "abcdef"s;  // std::string
+    auto atxt_3 = "abcdef"sv; // std::string_view
+
+    print(atxt_1);
+    print(atxt_2);
+    print(atxt_3);
+
+    const char* path = R"(c:\nasz katalog\todo)";
+    print(path);
+
+    const char* multi_line_text_1 = R"(line1
+line2
+line3)";
+
+    auto multi_line_text_2 = R"raw(line1
+line2
+line3)raw"s;
+
+    REQUIRE(multi_line_text_1 == multi_line_text_2);
 }
