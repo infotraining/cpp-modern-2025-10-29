2da1785b3cce308d02e0efc4184a4d78faa3c64e
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-29 13:52:45 +0100

    Refactor auto type deduction examples and remove commented code

--- _exercises/ex_auto_declarations/auto_declarations_ex.cpp
+++ _exercises/ex_auto_declarations/auto_declarations_ex.cpp
@@ -12,61 +12,66 @@ const vector<int> cvec = {1, 2, 3};
 
 using TODO = void;
 
+const int* ptr_to_const = &x;
+int const* ptr_to_const = &x;
+int* const const_ptr = &x;
+const int* const const_ptr_to_const = &x;
+int const* const const_ptr_to_const = &x;
+
 int main()
 {
+    auto a1 = x;
+    static_assert(is_same<int, decltype(a1)>::value, "Error");
 
-    // auto a1 = x;
-    // static_assert(is_same<TODO, decltype(a1)>::value, "Error");
+    auto a2 = &x;
+    static_assert(is_same<int*, decltype(a2)>::value, "Error");
 
-    // auto a2 = &x;
-    // static_assert(is_same<TODO, decltype(a2)>::value, "Error");
+    const auto a3 = &x;
+    static_assert(is_same<int* const, decltype(a3)>::value, "Error");
 
-    // const auto a3 = &x;
-    // static_assert(is_same<TODO, decltype(a3)>::value, "Error");
+    auto* a4 = &x;
+    static_assert(is_same<int*, decltype(a4)>::value, "Error");
 
-    // auto* a4 = &x;
-    // static_assert(is_same<TODO, decltype(a4)>::value, "Error");
+    auto a5 = &cx;
+    static_assert(is_same<const int*, decltype(a5)>::value, "Error");
 
-    // auto a5 = &cx;
-    // static_assert(is_same<TODO, decltype(a5)>::value, "Error");
+    auto* a6 = &cx;
+    static_assert(is_same<const int*, decltype(a6)>::value, "Error");
 
-    // auto* a6 = &cx;
-    // static_assert(is_same<TODO, decltype(a6)>::value, "Error");
+    int& rx = x;
+    const int& crx = x;
 
-    // int& rx = x;
-    // const int& crx = x;
+    const auto& a7 = rx;
+    static_assert(is_same<const int&, decltype(a7)>::value, "Error");
 
-    // const auto& a7 = rx;
-    // static_assert(is_same<TODO, decltype(a7)>::value, "Error");
+    auto a8 = crx;
+    static_assert(is_same<int, decltype(a8)>::value, "Error");
 
-    // auto a8 = crx;
-    // static_assert(is_same<TODO, decltype(a8)>::value, "Error");
+    auto& a9 = crx;
+    static_assert(is_same<const int&, decltype(a9)>::value, "Error");
 
-    // auto& a9 = crx;
-    // static_assert(is_same<TODO, decltype(a9)>::value, "Error");
+    auto a10 = tab;
+    static_assert(is_same<int*, decltype(a10)>::value, "Error");
 
-    // auto a10 = tab;
-    // static_assert(is_same<TODO, decltype(a10)>::value, "Error");
+    auto& a11 = tab;
+    static_assert(is_same<int(&)[10], decltype(a11)>::value, "Error");
 
-    // auto& a11 = tab;
-    // static_assert(is_same<TODO, decltype(a11)>::value, "Error");
+    auto a12 = vec.begin();
+    static_assert(is_same<std::vector<int>::iterator, decltype(a12)>::value, "Error");
 
-    // auto a12 = vec.begin();
-    // static_assert(is_same<TODO, decltype(a12)>::value, "Error");
+    auto a13 = vec.cbegin();
+    static_assert(is_same<std::vector<int>::const_iterator, decltype(a13)>::value, "Error");
 
-    // auto a13 = vec.cbegin();
-    // static_assert(is_same<TODO, decltype(a13)>::value, "Error");
+    auto a14 = vec.back();
+    static_assert(is_same<int, decltype(a14)>::value, "Error");
 
-    // auto a14 = vec.back();
-    // static_assert(is_same<TODO, decltype(a14)>::value, "Error");
+    auto& a15 = cvec.back();
+    static_assert(is_same<const int&, decltype(a15)>::value, "Error");
 
-    // auto& a15 = cvec.back();
-    // static_assert(is_same<TODO, decltype(a15)>::value, "Error");
+    const auto a16 = vec[0];
+    static_assert(is_same<const int, decltype(a16)>::value, "Error");
 
-    // const auto a16 = vec[0];
-    // static_assert(is_same<TODO, decltype(a16)>::value, "Error");
-
-    ///////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
 
     // auto&& a17 = 7.5;
     // static_assert(is_same<TODO, decltype(a17)>::value, "Error");
@@ -77,6 +82,6 @@ int main()
     // auto&& a19 = vec.front();
     // static_assert(is_same<TODO, decltype(a19)>::value, "Error");
 
-    //auto&& a20 = vec.begin();
-    //static_assert(is_same<TODO, decltype(a20)>::value, "Error");
+    // auto&& a20 = vec.begin();
+    // static_assert(is_same<TODO, decltype(a20)>::value, "Error");
 }
