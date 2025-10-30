f80b8e5d3fb139c267b947813621d870b229370b
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-30 10:49:17 +0100

    initializer_list

--- core-features/tests_uniform_init_syntax.cpp
+++ core-features/tests_uniform_init_syntax.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <variant>
 
 struct Aggregate
 {
@@ -89,10 +90,127 @@ TEST_CASE("uniform init syntax")
     }
 }
 
+template <typename T>
+class Container
+{
+    T* items_;
+    size_t size_;
+public:
+    using iterator = T*;
+    using const_iterator = const T*;
+
+    Container(size_t size, const T& default_value)
+    {
+        size_ = size;
+        items_ = new T[size_];
+        std::fill_n(items_, size_, default_value); 
+    }
+    
+    Container(std::initializer_list<T> lst)
+    {
+        size_ = lst.size();
+        items_ = new T[size_]; 
+        std::copy(lst.begin(), lst.end(), items_);
+    }
+
+    ~Container()
+    {
+        delete[] items_;
+    }
+
+    size_t size() const
+    {
+        return size_;
+    }
+
+    iterator begin()
+    {
+        return items_;
+    }
+
+    iterator end()
+    {
+        return items_ + size_;
+    }
+
+    const_iterator begin() const
+    {
+        return items_;
+    }
+
+    const_iterator end() const
+    {
+        return items_ + size_;
+    }
+
+    T& operator[](size_t index)
+    {
+        return items_[index];
+    }
+
+    const T& operator[](size_t index) const
+    {
+        return items_[index];
+    }
+};
+
+template <typename T, size_t N>
+struct Array
+{
+    T items[N];
+};
+
 TEST_CASE("initializer_list")
 {
     std::initializer_list<int> lst_1 = {1, 2, 3, 4, 5};
     auto lst_2 = {1, 2, 3, 4, 5};
 
     auto numbers = {1.0, 2.3, 3.44, static_cast<double>(9)};
+
+    Container container = {1, 2, 3, 4, 5, 6, 7};
+    REQUIRE(container[3] == 4);
+
+    std::vector expected = {1, 2, 3, 4, 5, 6, 7};
+    REQUIRE(std::equal(container.begin(), container.end(), expected.begin(), expected.end()));
+
+    Array<int, 10> arr = {1, 2, 3};
+}
+
+TEST_CASE("constructor with initializer")
+{
+    Container<int> c1 = {10, 20};
+    REQUIRE(c1.size() == 2);
+
+    Container<int> c2{10, 20};
+    REQUIRE(c2.size() == 2);
+
+    Container<int> c3(10, 20);
+    REQUIRE(c3.size() == 10);
+
+    std::vector<int> vec1 = {10, 20};
+    REQUIRE(vec1 == std::vector{10, 20});
+
+    std::vector<int> vec2(10, 20);
+    REQUIRE(vec2 == std::vector{20, 20, 20, 20, 20, 20, 20, 20, 20, 20});
+}
+
+// class VariantContainer
+// {
+//     std::variant<int,std::string>* items_;
+//     size_t size_;
+// public:
+//     VariantContainer(std::initializer_list<std::variant<int,std::string>> lst)
+//     {
+//         size_ = lst.size();
+//         items_ = new std::variant<int,std::string>[size_]; 
+//         std::copy(lst.begin(), lst.end(), items_);
+//     }
+// };
+
+TEST_CASE("initializer_list variant")
+{
+    std::initializer_list<std::variant<int,std::string>> variant_1{1,"1"};
+    std::initializer_list<std::variant<int,std::string>> variant_2 = {1,"1"};
+
+    Container<std::variant<int, std::string>> container = {1, "2.2", 3, 4, 5, 6};
 }
